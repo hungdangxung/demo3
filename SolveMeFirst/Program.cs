f3421943a092c0a2e461dfb72679d2d7bd3f6971
hungdangxung <120209106+hungdangxung@users.noreply.github.com>
2023-01-16 10:21:28 +0700

    Add SimpleArraySum

---
+++ SolveMeFirst/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static int solveMeFirst(int a, int b)
+    {
+        if (((1 <= a) && (a <= 1000)) && ((1 <= b) && (b <= 1000)))
+        {
+            return a + b;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    static void Main(String[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Nhập a: ");
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Nhập b: ");
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = solveMeFirst(val1, val2);
+        Console.WriteLine(sum);
+        Console.ReadKey();
+    }
+}
